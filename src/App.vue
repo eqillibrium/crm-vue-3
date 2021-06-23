6ab7936f9854f594db39cd935bbe29980c486cc5
Eqillibrium <eqillibrium@live.ru>
2021-06-23 09:34:41 +0300

    added routes and views

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
-  <div id="app">
-    <button class="btn">test</button>
-    <router-view/>
-  </div>
+  <component :is="`${layout}-layout`"></component>
 </template>
 
 <style lang="scss">
   @import '~materialize-css/dist/css/materialize.min.css';
   @import 'assets/index.css';
 </style>
+
+<script>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import AuthLayout from './layouts/AuthLayout'
+import MainLayout from './layouts/MainLayout'
+
+export default {
+  setup () {
+    const route = useRoute()
+    return {
+      layout: computed(() => route.meta.layout)
+    }
+  },
+  components: { AuthLayout, MainLayout }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,79 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home'
 
 const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: Home,
+    meta: {
+      layout: 'main'
+    }
+  },
+  {
+    name: 'Categories',
+    path: '/categories',
+    component: () => import('../views/Categories'),
+    meta: {
+      layout: 'main'
+    }
+  },
+  {
+    name: 'Detail',
+    path: '/detail',
+    component: () => import('../views/Detail'),
+    meta: {
+      layout: 'main'
+    }
+  },
+  {
+    name: 'History',
+    path: '/history',
+    component: () => import('../views/History'),
+    meta: {
+      layout: 'main'
+    }
+  },
+  {
+    name: 'Planning',
+    path: '/planning',
+    component: () => import('../views/Planning'),
+    meta: {
+      layout: 'main'
+    }
+  },
+  {
+    name: 'Profile',
+    path: '/profile',
+    component: () => import('../views/Profile'),
+    meta: {
+      layout: 'main'
+    }
+  },
+  {
+    name: 'Record',
+    path: '/record',
+    component: () => import('../views/Record'),
+    meta: {
+      layout: 'main'
+    }
+  },
+  {
+    name: 'auth',
+    path: '/auth',
+    component: () => import('../views/Login'),
+    meta: {
+      layout: 'auth'
+    }
+  },
+  {
+    name: 'register',
+    path: '/register',
+    component: () => import('../views/Register'),
+    meta: {
+      layout: 'auth'
+    }
+  }
 ]
 
 const router = createRouter({
